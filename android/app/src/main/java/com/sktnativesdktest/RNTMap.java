9dc61a961ecc867e3d9750f572e31231775e1684
Nikolas Vasconcelos <nkvdamian@gmail.com>
2021-04-12 20:57:44 -0300

    Display Map on Screen [WiP]

--- android/app/src/main/java/com/sktnativesdktest/RNTMap.java
+++ android/app/src/main/java/com/sktnativesdktest/RNTMap.java
@@ -1,18 +1,13 @@
 package com.sktnativesdktest;
 
-import android.util.Log;
-import android.widget.LinearLayout;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
-import com.facebook.react.uimanager.ViewProps;
 import com.facebook.react.uimanager.annotations.ReactProp;
-import com.skt.Tmap.*;
+import com.skt.Tmap.TMapView;
 
 public class RNTMap extends SimpleViewManager<TMapView> {
 
@@ -26,7 +21,17 @@ public class RNTMap extends SimpleViewManager<TMapView> {
   @NonNull
   @Override
   protected TMapView createViewInstance(@NonNull ThemedReactContext reactContext) {
-    return new TMapView(reactContext, 37.570841, 126.988205, 16);
+    try {
+      TMapView tMapView = new TMapView(reactContext, 37.570841, 126.988205, 16);
+      tMapView.setMapType(TMapView.MAPTYPE_STANDARD);
+      tMapView.setSKTMapApiKey("l7xx9d4d587fe7104a57b8feda886c846d1f");
+      tMapView.setHttpsMode(false);
+      tMapView.initView();
+      return tMapView;
+    } catch (Exception e) {
+      e.printStackTrace();
+      return null;
+    }
   }
 
   @Override
@@ -36,16 +41,25 @@ public class RNTMap extends SimpleViewManager<TMapView> {
 
   @ReactProp(name = "appKey")
   public void setAppKey(TMapView view, @Nullable String appKey) {
+    if (view != null) {
+      view = new TMapView(this.mCallerContext, 37.570841, 126.988205, 16);
+    }
     view.setSKTMapApiKey(appKey);
   }
 
   @ReactProp(name = "lat", defaultDouble = 126.988205)
   public void setLatitude(TMapView view, double lat) {
+    if (view != null) {
+      view = new TMapView(this.mCallerContext, 37.570841, 126.988205, 16);
+    }
     view.setCenterPoint(view.getLongitude(), lat);
   }
 
   @ReactProp(name = "lng", defaultDouble = 126.988205)
   public void setLongitude(TMapView view, @Nullable double lng) {
+    if (view != null) {
+      view = new TMapView(this.mCallerContext, 37.570841, 126.988205, 16);
+    }
     view.setCenterPoint(lng, view.getLatitude());
   }
 }
--- android/app/src/main/java/com/sktnativesdktest/RNTMapPackage.java
+++ android/app/src/main/java/com/sktnativesdktest/RNTMapPackage.java
@@ -4,8 +4,8 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class RNTMapPackage implements ReactPackage {
@@ -19,6 +19,8 @@ public class RNTMapPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(
             ReactApplicationContext reactContext) {
-        return Collections.emptyList();
+        List<NativeModule> modules = new ArrayList<>();
+        modules.add(new RNTMap(reactContext));
+        return modules;
     }
 }
